2d3267261e9f369777bb8ff3b61c9368828f7fcd
lxfu1 <954055752@qq.com>
2026-06-18 20:52:39 +0800

    feat: csp 更新

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 
+function removeCrossorigin() {
+  return {
+    name: 'remove-crossorigin',
+    transformIndexHtml(html) {
+      return html.replace(/ crossorigin/g, '');
+    },
+  };
+}
+
 export default defineConfig({
   clearScreen: false,
   server: {
     port: 1420,
     strictPort: true,
   },
+  plugins: [removeCrossorigin()],
   envPrefix: ['VITE_', 'TAURI_'],
   build: {
     target: 'esnext',
